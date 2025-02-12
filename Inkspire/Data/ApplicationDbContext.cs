b1db55985d58512f67119099d87008f8c9ac45b3
SdHinokamiKagura <satyad123.git.hub.in@gmail.com>
2025-02-13 00:06:16 +0530

    Add project files.

---
+++ Inkspire/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inkspire.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
---
+++ Inkspire/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inkspire.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
