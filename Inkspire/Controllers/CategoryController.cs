9bca467f92774c13dfd09a1ae09671c64465424b
SdHinokamiKagura <satyad123.git.hub.in@gmail.com>
2025-02-13 12:39:21 +0530

    Added Category Model to Db and seeded data related to it

---
+++ Inkspire/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Inkspire.Data;
+using Inkspire.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inkspire.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.categories.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- Inkspire/Data/ApplicationDbContext.cs
+++ Inkspire/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Inkspire.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Inkspire.Data
 {
@@ -6,6 +7,17 @@ namespace Inkspire.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
+        }
+        public DbSet<Category> categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "War", DisplayOrder=1 },
+                new Category { Id = 2, Name = "History", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Comics", DisplayOrder = 3 }
+                );
+
 
         }
 
