f7f4a0f3e593255f8e4fb5948cec4e799bae0087
SdHinokamiKagura <satyad123.git.hub.in@gmail.com>
2025-02-14 00:05:21 +0530

    Server side,custom side validations are added here and skipped client side validation and also added asp-validation summary for all and ModelOnly

--- Inkspire/Controllers/CategoryController.cs
+++ Inkspire/Controllers/CategoryController.cs
@@ -20,5 +20,33 @@ namespace Inkspire.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+            //present Not Needed
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+
+            //    //if you dont mention the property in place of key of  ModelState.AddModelError(key,errorMessage) and kept empty
+            //    ModelState.AddModelError("", "Display Order and Name should not be same");
+            //    //error is shown on top for complete form and you have to mention {asp-validation-for=ModelOnly} in <div> tag
+
+            //    //if you mention the property in place of key of  ModelState.AddModelError(key,errorMessage)
+            //    //ModelState.AddModelError("Name", "Display Order and Name should not be same");
+            //    //error is shown below the input element individually
+            //}
+
+        }
     }
 }
