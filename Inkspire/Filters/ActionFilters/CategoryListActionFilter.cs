20ddb957048e291a8cfdc5e5ff86eb973b47acff
SdHinokamiKagura <satyad123.git.hub.in@gmail.com>
2025-03-12 22:42:55 +0530

    Action_Filter

---
+++ Inkspire/Filters/ActionFilters/CategoryListActionFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Inkspire.Filters.ActionFilters
+{
+    public class CategoryListActionFilter : IActionFilter
+    {
+        private readonly ILogger<CategoryListActionFilter> _logger;
+
+        public CategoryListActionFilter(ILogger<CategoryListActionFilter> logger)
+        {
+            _logger = logger;
+        }
+        //After
+        public void OnActionExecuted(ActionExecutedContext context)
+        { 
+            _logger.LogInformation("CategoryList Action Filter : OnActionExecuted");
+        }
+
+        //Before
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("CategoryList Action Filter : OnActionExecuting");
+        }
+    }
+}
